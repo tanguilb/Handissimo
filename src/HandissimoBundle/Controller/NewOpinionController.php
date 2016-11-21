6e6eedf1745208aadfa362d00b50d7523ddb99ee
Axelle-slv <axelle.silva@gmail.com>
2016-11-21 18:05:01 +0100

    creation of the button form that allow to give opinion on the website

---
+++ src/HandissimoBundle/Controller/NewOpinionController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: axcel
+ * Date: 21/11/16
+ * Time: 16:00
+ */
+
+namespace HandissimoBundle\Controller;
+
+use HandissimoBundle\Entity\Opinion;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class NewOpinionController extends Controller
+{
+    /**
+     * Creates a new opinion entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $opinion = new Opinion();
+        $form = $this->createForm('HandissimoBundle\Form\OpinionType', $opinion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($opinion);
+            $em->flush($opinion);
+        }
+
+        return $this->render('front/opinion-button.html.twig', array(
+            'opinion' => $opinion,
+            'form' => $form->createView(),
+        ));
+    }
+
+}
