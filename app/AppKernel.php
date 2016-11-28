792aaf6518fd95f824317e6698697988460eb6c5
Axelle-slv <axelle.silva@gmail.com>
2016-11-28 14:12:33 +0100

    Installation of Fos User Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
             new HandissimoBundle\HandissimoBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
