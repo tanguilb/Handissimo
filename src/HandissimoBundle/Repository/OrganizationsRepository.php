cafd50bf6d5813a3146740b3db59a1c38a312d95
tanguilb <tangui.lebourdonnec@gmail.com>
2016-12-06 14:01:19 +0100

    second inutil commit

--- src/HandissimoBundle/Repository/OrganizationsRepository.php
+++ src/HandissimoBundle/Repository/OrganizationsRepository.php
@@ -12,20 +12,20 @@ use Doctrine\ORM\EntityRepository;
 
 class OrganizationsRepository extends EntityRepository
 {
-    public function DisabilityGetByOrganizations($organizationData, $needsData, $disabilityData)
+    public function DisabilityGetByOrganizations($organizationData/*, $needsData, $disabilityData*/)
     {
         $organizationData = "%" . $organizationData . "%";
         $query = $this->createQueryBuilder('o')
-            ->innerJoin('o.needs', 'n')
-            ->innerJoin('o.disabilityTypes', 'dt')
-            ->addSelect('o.name', 'n.needName', 'dt.disabilityName')
+            //->innerJoin('o.needs', 'n')
+            //->innerJoin('o.disabilityTypes', 'dt')
+            ->addSelect('o.name'/*, 'n.needName', 'dt.disabilityName'*/)
             ->where('o.name LIKE :organizationData')
-            ->orWhere('n.needName LIKE :dataneeds')
-            ->orWhere('dt.disabilityName Like :disabilityData')
+           // ->orWhere('n.needName LIKE :dataneeds')
+           // ->orWhere('dt.disabilityName Like :disabilityData')
             ->orderBy('o.name', 'ASC')
             ->setParameter('organizationData', $organizationData)
-            ->setParameter('dataneeds', '%' .$needsData . '%' )
-            ->setParameter('disabilityData', '%' .$disabilityData . '%')
+            //->setParameter('dataneeds', '%' .$needsData . '%' )
+            //->setParameter('disabilityData', '%' .$disabilityData . '%')
             ->getQuery();
         return $query->getResult();
 
