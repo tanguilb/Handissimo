bc7bcbd551544f6a1b6682082745d3e3acf2b7ec
Ducruet david <david.ducruet@orange.fr>
2017-01-13 10:01:21 +0100

    wip

--- src/HandissimoBundle/Form/ResearchType.php
+++ src/HandissimoBundle/Form/ResearchType.php
@@ -3,12 +3,12 @@
 namespace HandissimoBundle\Form;
 
 
-use Sonata\AdminBundle\Form\Type\Filter\ChoiceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ResearchType extends AbstractType
@@ -30,42 +30,40 @@ class ResearchType extends AbstractType
                     array('attr' => array('autocomplete' => 'off'),
                     'required' => false,
                     'label' => false))
-            /*->add('disabilitytypes', EntityType::class, array(
-                'empty_value' => 'Sélectionner un type de handicaps',
-                'class' => 'HandissimoBundle:DisabilityTypes',
-                'choice_label' => 'disabilityName',
-                'label' => 'Type de handicaps',
-                'expanded' => false,
-                'multiple' => true,
-                'required' => false
-            ))*/
-            ->add('disabilitytypes', CollectionType::class, array(
-                'label' => 'Type de handicaps',
-                ' ' => true,
-                //'expanded' => false,
-                //'multiple' => true,
-                'required' => false
-            ))
-            ->add('needs', EntityType::class, array(
-                'empty_value' => 'Sélectionner un type de besoins',
-                'class' => 'HandissimoBundle\Entity\Needs',
-                'property' => 'needName',
-                'label' => 'Type de besoins',
-                'expanded' => false,
-                'multiple' => true,
-                'required' => false
-            ))
-            ->add('structurestypes', EntityType::class, array(
-                'empty_value' => 'Sélectionner un type de structures',
-                'class' => 'HandissimoBundle\Entity\StructuresTypes',
-                'property' => 'structurestype',
-                'label' => 'Type de Structures',
-                'expanded' => false,
-                'multiple' => true,
-                'required' => false
-            ))
             ->add('save', SubmitType::class,
-                    array('label' => 'Rechercher'));
+                array('label' => 'Rechercher'));
+
+        $builder->addEventListener(
+            FormEvents::SUBMIT,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+                $form
+                    ->add('disabilitytypes', EntityType::class, array(
+                        'empty_value' => 'Sélectionner un type de handicaps',
+                        'class' => 'HandissimoBundle:DisabilityTypes',
+                        'property' => 'disabilityName',
+                        'label' => 'Type de handicaps',
+                        'expanded' => false,
+                        'required' => false
+                    ))
+                    ->add('needs', EntityType::class, array(
+                        'empty_value' => 'Sélectionner un type de besoins',
+                        'class' => 'HandissimoBundle\Entity\Needs',
+                        'property' => 'needName',
+                        'label' => 'Type de besoins',
+                        'expanded' => false,
+                        'required' => false
+                    ))
+                    ->add('structurestypes', EntityType::class, array(
+                        'empty_value' => 'Sélectionner un type de structures',
+                        'class' => 'HandissimoBundle\Entity\StructuresTypes',
+                        'property' => 'structurestype',
+                        'label' => 'Type de Structures',
+                        'expanded' => false,
+                        'required' => false
+                    ));
+            }
+        );
     }
 
     /**
