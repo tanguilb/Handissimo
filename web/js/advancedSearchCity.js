67220a6fb8d7b3759a5448ac70aaa38ec0aa0bb7
tanguilb <tangui.lebourdonnec@gmail.com>
2017-01-19 17:22:43 +0100

    add autocoplete on the advanced search

---
+++ web/js/advancedSearchCity.js
@@ -0,0 +1,34 @@
+/**
+ * Created by tangui on 19/01/17.
+ */
+$( document ).ready(function() {
+    $("#research_advanced_postal").keyup(function(){
+        var postalcode = $(this).val();
+        if ( postalcode.length >= 2 ) {
+            $.ajax({
+                type: "POST",
+                url: "/research/ajaxcity/" + postalcode,
+                dataType: 'json',
+                timeout: 3000,
+                success: function(response){
+                    var postalcodes = JSON.parse(response.data);
+                    var html = "";
+                    for (var i = 0; i < postalcodes.length; i++) {
+                        html += "<li>" + postalcodes[i].postal + "</li>";
+                    }
+
+                    $('#city').html(html);
+                    $('#city').find('li').on('click', function() {
+                        $('#research_advanced_postal').val($(this).text());
+                        $('#city').html('');
+                    });
+                },
+                error: function() {
+                    $('#city').text('Ajax call error');
+                }
+            });
+        } else {
+            $('#city').html('');
+        }
+    });
+});
