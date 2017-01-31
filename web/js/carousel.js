e0eec97c60bee88aa6b180ee83965bf817d6ab55
Axelle-slv <axelle.silva@gmail.com>
2017-01-31 15:14:44 +0100

    I reorganized the twig and the css of some pages

--- web/js/carousel.js
+++ web/js/carousel.js
@@ -1,6 +1,6 @@
 setInterval(function () {
 
-    $('.carousel ul').animate({marginLeft:'-400px'}, 4000, function () {
+    $('.carousel ul').animate({marginLeft:'-453px'}, 4000, function () {
         /*$(this).find("li:last").after($(this).find("li:first"));*/
         $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
         /*$(this).css({marginleft: 0});*/
