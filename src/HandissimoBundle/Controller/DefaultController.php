e400d61d7ba915dc153443248e3e2b7e5adb01fc
tanguilb <tangui.lebourdonnec@gmail.com>
2017-02-20 13:56:59 +0100

    change action for organization page in the default controller

--- src/HandissimoBundle/Controller/DefaultController.php
+++ src/HandissimoBundle/Controller/DefaultController.php
@@ -2,8 +2,10 @@
 
 namespace HandissimoBundle\Controller;
 
+use HandissimoBundle\Entity\Organizations;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -20,5 +22,14 @@ class DefaultController extends Controller
     public function aboutAction(){
         return $this->render('front/about.html.twig');
     }
+    public function standardPageAction(Organizations $organization){
+        $user = $this->getUser();
+        $organization = $this->get('templating')
+            ->render('front/organizationPage.html.twig', array(
+                'organization' => $organization,
+                'user' => $user));
 
+        return new Response($organization);
+
+    }
 }
