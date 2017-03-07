20acaf1c0f3b3b0af956e1ac09abc9cb20d5addc
Ducruet david <david.ducruet@orange.fr>
2017-03-07 16:48:25 +0100

    add option for translation message captcha

--- web/js/vote.js
+++ web/js/vote.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(".vote-like").on('click', function () {
+    /*$(".vote-like").on('click', function () {
         var like = $(this).val();
         alert(like);
         $.ajax({
@@ -25,7 +25,17 @@ $(document).ready(function () {
 
             }
         })
-    })
+    })*/
+
+    function likeFunction(){
+        var value = $("#").val();
+        $.post("myModuleName/myAction",
+            {myParam : value},
+            function(data)
+            {
+                $(".vote-like").attr("value",data);
+            });
+    }
 
 });
 
