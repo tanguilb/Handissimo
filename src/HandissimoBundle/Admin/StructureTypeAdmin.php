20231f7900ac5dc0c25747eb0497467d7f1acd79
tanguilb <tangui.lebourdonnec@gmail.com>
2017-03-28 16:47:45 +0200

    add crud action for organizations

---
+++ src/HandissimoBundle/Admin/StructureTypeAdmin.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace HandissimoBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class StructureTypeAdmin extends AbstractAdmin
+{
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('id')
+            ->add('name')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->add('name')
+            ->add('_action', null, array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', TextType::class, array(
+                'label' => 'Type de structure',
+                'required' => false,
+            ))
+        ;
+    }
+
+    /**
+     * @param ShowMapper $showMapper
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('id')
+            ->add('name')
+        ;
+    }
+}
--- src/HandissimoBundle/Controller/OrganizationsController.php
+++ src/HandissimoBundle/Controller/OrganizationsController.php
@@ -1,36 +1,105 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tangui
- * Date: 27/03/17
- * Time: 09:38
- */
 
 namespace HandissimoBundle\Controller;
 
-use Exporter\Handler;
-use HandissimoBundle\Form\Handler\OrganizationsHandler;
 use HandissimoBundle\Entity\Organizations;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Organization controller.
+ *
+ */
 class OrganizationsController extends Controller
 {
+    /**
+     * Creates a new organization entity.
+     *
+     */
     public function newAction(Request $request)
     {
-        $organizations = new Organizations();
-        $form = $this->createForm('HandissimoBundle\Form\Type\OrganizationsType', $organizations);
+        $organization = new Organizations();
+        $form = $this->createForm('HandissimoBundle\Form\Type\OrganizationsType', $organization);
+        $form->handleRequest($request);
 
-        $formHandler = new OrganizationsHandler($form, $request, $this->get('doctrine.orm.default_entity_manager'));
-        if($formHandler->process())
-        {
-            $this->addFlash('notice', 'Vous avez bien enregistrer vos informations');
-            return $this->redirectToRoute('handissimobundle_organizations');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($organization);
+            $em->flush($organization);
+
+            return $this->redirectToRoute('handissimo_organizations_standard_page', array('id' => $organization->getId()));
         }
 
-        return $this->render(':front:organizationForm.html.twig', array(
+        return $this->render('organizations/new.html.twig', array(
+            'organization' => $organization,
             'form' => $form->createView(),
         ));
     }
 
+    /**
+     * Displays a form to edit an existing organization entity.
+     *
+     */
+    public function editAction(Request $request, Organizations $organization)
+    {
+        $deleteForm = $this->createDeleteForm($organization);
+        $editForm = $this->createForm('HandissimoBundle\Form\Type\OrganizationsType', $organization);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('structure_page', array('id' => $organization->getId()));
+        }
+
+        return $this->render('organizations/edit.html.twig', array(
+            'organization' => $organization,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a organization entity.
+     *
+     */
+    public function deleteAction(Request $request, Organizations $organization)
+    {
+        $form = $this->createDeleteForm($organization);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($organization);
+            $em->flush($organization);
+        }
+
+        return $this->redirectToRoute('research_action');
+    }
+
+    /**
+     * Creates a form to delete a organization entity.
+     *
+     * @param Organizations $organization The organization entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Organizations $organization)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('organizations_delete', array('id' => $organization->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+
+    private function draftCopyAction(Organizations $organization)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $copy = clone $organization;
+        $em->persist($copy);
+        $em->flush();
+        return $this->redirectToRoute('handissimo_structure');
+
+    }
 }
