01e078b78b824e77712852eb44c7de03ef37c7b2
Ducruet david <david.ducruet@orange.fr>
2017-04-28 18:06:06 +0200

    add modification for page check-email

--- web/js/search.js
+++ web/js/search.js
@@ -12,5 +12,6 @@ $(function () {
 
     $('#btn-compact').on('click', function () {
         document.getElementById('front_user_registration_form_compact').checked = true;
+        document.getElementById('submit-register').removeAttribute('disabled');
     })
 });
