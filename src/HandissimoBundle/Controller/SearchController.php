313aa848941d512df6b591f686bc41e368d97745
tanguilb <tangui.lebourdonnec@gmail.com>
2017-05-18 16:23:58 +0200

    add function to sort result by search correspondig and distance and create the associated service

---
+++ src/HandissimoBundle/Controller/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tangui
+ * Date: 18/05/17
+ * Time: 14:17
+ */
+
+namespace HandissimoBundle\Controller;
+
+
+class SearchController
+{
+
+}
---
+++ src/HandissimoBundle/Services/Search/SortSearch.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: tangui
+ * Date: 18/05/17
+ * Time: 14:22
+ */
+class SortSearch
+{
+
+}
