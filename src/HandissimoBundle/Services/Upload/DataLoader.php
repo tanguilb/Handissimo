68af700c6adbb0e0dce31edd87cc6c1a20c31091
Ducruet david <david.ducruet@orange.fr>
2017-05-22 10:41:49 +0200

    add console command for load data for the database

---
+++ src/HandissimoBundle/Services/Upload/DataLoader.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: david
+ * Date: 21/05/17
+ * Time: 10:52
+ */
+
+namespace HandissimoBundle\Services\Upload;
+
+use Doctrine\ORM\EntityManager;
+use HandissimoBundle\Entity\Organizations;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class DataLoader
+{
+
+    protected $em;
+    protected $container;
+
+    public function __construct(EntityManager $em, ContainerInterface $container)
+    {
+        $this->em = $em;
+        $this->container = $container;
+    }
+
+    public function loadAction($name)
+    {
+        $string = file_get_contents($this->container->get('kernel')->getRootDir()."/../".$name);
+        $contents = json_decode($string, true);
+        //$em = $this->getDoctrine()->getManager();
+        foreach ($contents as $content){
+            $organizationsEntity = new Organizations();
+            $organizationsEntity->setName($content['name']);
+            $organizationsEntity->setAddress($content['address']);
+            //$organizationsEntity->setAddressComplement($content['addressComplement']);
+            $organizationsEntity->setPostal($content['postal']);
+            $organizationsEntity->setCity($content['city']);
+            $organizationsEntity->setPhoneNumber($content['phoneNumber']);
+            $organizationsEntity->setMail($content['email']);
+            //$organizationsEntity->setWebsite($content['website']);
+            $organizationsEntity->setOrgaStructure($this->em->getReference('HandissimoBundle:StructuresList', 114));
+            //$organizationsEntity->setFreeplace($test['freeplace']);
+            //$organizationsEntity->setAgemini($test['agemini']);
+            //$organizationsEntity->setAgemaxi($test['agemaxi']);
+            //$organizationsEntity->setDirectorName($test['directorName']);
+            $this->em->persist($organizationsEntity);
+        }
+        $this->em->flush();
+        //$this->render(":front:about.html.twig");
+    }
+
+}
