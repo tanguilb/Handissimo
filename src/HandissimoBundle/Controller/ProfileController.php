863000969a50c405ed528fc2537197aa283b2344
Ducruet david <david.ducruet@orange.fr>
2017-06-06 11:19:40 +0200

    add page for listing all contribute cards by user

--- src/HandissimoBundle/Controller/ProfileController.php
+++ src/HandissimoBundle/Controller/ProfileController.php
@@ -44,6 +44,45 @@ class ProfileController extends Controller
         return $this->render('front/profile/profile-solution.html.twig');
     }
 
+    public function listUserCardAction()
+    {
+        $user = $this->container->get('security.token_storage')->getToken()->getUser()->getUsernameCanonical();
+        $em = $this->getDoctrine()->getManager();
+
+        /**
+         * Action for recovery all contributions for organizations by user
+         */
+        $query = 'SELECT * FROM organizations_audit WHERE organizations_audit.user = "' . $user . '" GROUP BY organizations_audit.id ORDER BY organizations_audit.update_datetime DESC';
+        $statement = $em->getConnection()->prepare($query);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $arrayId=[];
+        for ($key = 0; $key < count($result); $key++){
+            array_push($arrayId, $result[$key]['id']);
+        }
+        $organizations=[];
+        for ($k = 0; $k < count($arrayId); $k++){
+            array_push($organizations, $this->getDoctrine()->getRepository('HandissimoBundle:Organizations')->find($arrayId[$k]));
+        }
+
+        /**
+         * Method for recovery all contributions deleted by user
+         */
+        $queryTwo = 'SELECT * FROM organizations_audit WHERE organizations_audit.user = "' . $user . '" AND organizations_audit.revtype = "DEL" GROUP BY organizations_audit.id ORDER BY organizations_audit.update_datetime DESC';
+        $statementTwo = $em->getConnection()->prepare($queryTwo);
+        $statementTwo->execute();
+        $deletedOrganizations = $statementTwo->fetchAll();
+        $deletedResult =[];
+        for ($key = 0; $key < count($deletedOrganizations); $key++){
+            array_push($deletedResult, $deletedOrganizations[$key]['name']);
+        }
+
+        return $this->render('front/profile/profile-list-user-card.html.twig', array(
+            'organizations' => $organizations,
+            'deletedResult' => $deletedResult
+        ));
+    }
+
     public function profileCommentAction()
     {
         $user = $this->container->get('security.token_storage')->getToken()->getUser()->getUsernameCanonical();
@@ -54,11 +93,10 @@ class ProfileController extends Controller
         ));
     }
 
-    public function organizationsVersionAction(Organizations $organizations)
+    public function organizationsVersionAction($id)
     {
-        $organisationId = $organizations->getId();
         $em = $this->getDoctrine()->getManager();
-        $query = 'SELECT * FROM organizations_audit WHERE organizations_audit.id = ' . $organisationId . ' ORDER BY organizations_audit.update_datetime DESC';
+        $query = 'SELECT * FROM organizations_audit WHERE organizations_audit.id = ' . $id . ' ORDER BY organizations_audit.update_datetime DESC';
         $statement = $em->getConnection()->prepare($query);
         $statement->execute();
         $result = $statement->fetchAll();
@@ -98,10 +136,9 @@ class ProfileController extends Controller
         ));
     }
 
-    public function viewCurrentOrganizationsAction(Organizations $organizations)
+    public function viewCurrentOrganizationsAction($id)
     {
-        $organisationId = $organizations->getId();
-        $result = $this->getDoctrine()->getRepository('HandissimoBundle:Organizations')->find($organisationId);
+        $result = $this->getDoctrine()->getRepository('HandissimoBundle:Organizations')->find($id);
 
         return $this->render(':front/profile:profile-view-detail.html.twig', array(
             'result' => $result
@@ -120,6 +157,22 @@ class ProfileController extends Controller
 
         return $this->render(':front/profile:profile-list-deletion.html.twig', array(
             'pagination' => $pagination,
+        ));
+    }
+
+    public function deletionDetailAction($rev)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $query = 'SELECT * FROM organizations_audit WHERE organizations_audit.rev = ' . $rev . ' ORDER BY organizations_audit.update_datetime DESC';
+        $statement = $em->getConnection()->prepare($query);
+        $statement->execute();
+        $array = $statement->fetchAll();
+        $resultSql = $array[0];
+        $auditReader = $this->container->get('simplethings_entityaudit.reader');
+        $result = $auditReader->find(Organizations::class, $resultSql['id'], $resultSql['rev']);
+
+        return $this->render(':front/profile:profile-detail-deletion.html.twig', array(
+            'result' => $result,
         ));
     }
 }
