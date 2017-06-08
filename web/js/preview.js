42a182085c2bc74536ecdd7d084eacd86b4a316a
tanguilb <tangui.lebourdonnec@gmail.com>
2017-06-08 16:49:55 +0200

    add button to preview on new organizations

---
+++ web/js/preview.js
@@ -0,0 +1,14 @@
+/**
+ * Created by tangui on 08/06/17.
+ */
+$(document).ready(function(){
+    $('#preview').click(function(){
+
+        var data = {};
+        data['name'] = $('#handissimobundle_organizations_name').val();
+        data['gestionnaire'] = $('#handissimobundle_organizations_society').val();
+        data['']
+        console.log(data);
+    })
+
+});
